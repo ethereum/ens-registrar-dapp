3bbd7896fc4700cbb5c4ed3cdae897f3da0e9de4
Fernando Tiberti <fertiberti@gmail.com>
2016-12-05 13:51:05 -0300

    Use TemplateVar instead of Session

--- app/client/templates/components/nameStatus.js
+++ app/client/templates/components/nameStatus.js
@@ -1,26 +1,24 @@
 Template['components_nameStatus'].onCreated(function() {
-    // status update interval
-    var self = this;
-    Session.set('status', 'Looking up status...');
+    var template = this;
+    TemplateVar.set('status', 'Looking up status...');
     function lookUp() {
-      ens.resolver(self.data.name, (err, res) => {
-          Session.set('status', err ?
+      ens.resolver(template.data.name, (err, res) => {
+          TemplateVar.set(template, 'status', err ?
             String(err.message || err) :
             String(res)
           );
       });
     }
     lookUp();
-    this.updateStatus = Meteor.setInterval(lookUp, 1 * 1000);
+    this.updateStatusInterval = Meteor.setInterval(lookUp, 1 * 1000);
 });
 
 Template['components_nameStatus'].onDestroyed(function() {
-    // clear the status update interval
-    Meteor.clearInterval(this.updateStatus);
+    Meteor.clearInterval(this.updateStatusInterval);
 });
 
 Template['components_nameStatus'].helpers({
     'status': function(){
-		    return Session.get('status');
+		    return TemplateVar.get('status');
     },
 });
