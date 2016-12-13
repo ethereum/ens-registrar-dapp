8e4966e808eb5e1583e1283e1ad4148a902fcb3c
Fernando Tiberti <fertiberti@gmail.com>
2016-12-12 21:35:32 -0300

    Identify ropsten testnet

--- app/client/lib/ethereum/ens.js
+++ app/client/lib/ethereum/ens.js
@@ -6,9 +6,13 @@ Session.setDefault('network', false);
 // CHECK FOR NETWORK
 web3.eth.getBlock(0, function(e, res){
     if(!e){
+      console.log('hash: ' + res.hash);
         switch(res.hash) {
+            case '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d':
+                Session.set('network', 'ropsten testnet');
+                break;
             case '0x0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303':
-                Session.set('network', 'test');
+                Session.set('network', 'morden testnet');
                 break;
             case '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3':
                 Session.set('network', 'main');
