0dcf8f8d0f2eb2d2b091ef0bffc4d9480663b70c
Fernando Tiberti <fertiberti@gmail.com>
2016-12-14 19:47:02 -0300

    getNameStatus uses global ens and registry
    It's more helpful to have those available globaly in these early stages

--- app/imports/get-name-status.js
+++ app/imports/get-name-status.js
@@ -1,9 +1,3 @@
-var ENS = require('ethereum-ens');
-var Registrar = require('eth-registrar-ens');
-
-var ens = new ENS(web3, '0x112234455c3a32fd11230c42e7bccd4a84e02010');
-var registrar = new Registrar(web3, ens.owner('eth'), 7, 'eth', ens.registry.address);
-
 export default function getNameStatus(name, callback) {
   //Two tasks: get address, and get entry.
   let tasks = 2;
