92fec97229ceb0a55a5757c690e39ef9e21f872a
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2016-12-15 19:40:34 -0200

    adds dynamic templates

--- app/client/templates/layout/header.js
+++ app/client/templates/layout/header.js
@@ -6,10 +6,13 @@ Template['layout_header'].events({
 
 Template['layout_header'].helpers({
   'backgroundDataUrl': function(){
-    var searched = Session.get('searched');
+    var searched = Session.get('name');
     if (!searched) {
       return 'none';
     }
     return GeoPattern.generate(searched).toDataUrl();
+  },
+  'returnedName': function() {
+    return Session.get('name');
   }
 });
