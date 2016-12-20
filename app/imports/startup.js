45aa196a3ee98617df89e7f6ffe6388c6b1ec8f8
Fernando Tiberti <fertiberti@gmail.com>
2016-12-20 18:18:59 -0300

    Move everything to /imports

---
+++ app/imports/startup.js
@@ -0,0 +1,22 @@
+import './lib/ethereum/1_web3Init.js';
+import './lib/ethereum/2_setNetwork.js';
+import './lib/helpers/helperFunctions.js';
+import './lib/helpers/templateHelpers.js';
+import './lib/appStart.js';
+
+import './stylesheets/main.less';
+
+import './templates/components/entry.html';
+import './templates/components/nameStatus.html';
+import './templates/components/nameStatus.js';
+import './templates/layout/body.html';
+import './templates/layout/body.js';
+import './templates/layout/header.html';
+import './templates/layout/header.js';
+import './templates/layout/main.html';
+import './templates/layout/notFound.html';
+import './templates/index.html';
+
+import './collections';
+import './index.js';
+import './meta.js';
