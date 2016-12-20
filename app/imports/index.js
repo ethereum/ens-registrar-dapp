1ac6d0801d8487658c14d1cf7039ab262d31ab72
Fernando Tiberti <fertiberti@gmail.com>
2016-12-20 18:56:23 -0300

    Remove some boilerplate stuff

--- app/imports/index.js
+++ app/imports/index.js
@@ -1,19 +1,3 @@
-// disconnect any meteor server
-if(location.host !== 'localhost:3000'
-   && location.host !== '127.0.0.1:3000'
-   && typeof MochaWeb === 'undefined')
-    Meteor.disconnect();
-
-
-// Set the default unit to ether
-if(!LocalStore.get('etherUnit'))
-    LocalStore.set('etherUnit', 'ether');
-
-
-// Set Session default values for components
-if (Meteor.isClient) {
-	Session.setDefault('balance', '0');
-}
 
 Meteor.startup(function() {
     // SET default language
@@ -44,6 +28,6 @@ Meteor.startup(function() {
         }
     });
 
-	// Set Meta Title
-	Meta.setTitle(TAPi18n.__("dapp.app.title"));
+  // Set Meta Title
+  Meta.setTitle(TAPi18n.__("dapp.app.title"));
 });
