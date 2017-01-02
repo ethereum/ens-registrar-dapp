5cbe79bae9b2ddde4f813c4811af78dd591d5997
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2017-01-02 17:59:48 -0200

    update owned template

--- app/imports/templates/status/owned.js
+++ app/imports/templates/status/owned.js
@@ -1,4 +1,6 @@
 Template['status-owned'].onCreated(function() {
+  TemplateVar.set(this, 'entryData', Template.instance().data.entry);
+
   var name = Template.instance().data.name;
   TemplateVar.set(this, 'owner', ens.owner(name))
   ens.resolver(name, (err, res) => {
@@ -6,6 +8,8 @@ Template['status-owned'].onCreated(function() {
       TemplateVar.set(this, 'address', res.addr());
     }
   });
+
+  console.log('entry', Template.instance().data.entry);
 });
 
 Template['status-owned'].helpers({
@@ -14,5 +18,15 @@ Template['status-owned'].helpers({
   },
   owner() {
     return TemplateVar.get('owner')
+  },
+  registrationDate() {
+    console.log('date', TemplateVar.get('entryData').registrationDate );
+    var date = new Date(TemplateVar.get('entryData').registrationDate * 1000);
+    return date.toLocaleString();
+  },
+  deedValue() {
+    console.log('deedValue', TemplateVar.get('entryData'));
+    var val = TemplateVar.get('entryData').deed.balance;
+    return web3.fromWei(val.toFixed(), 'ether');
   }
 })
