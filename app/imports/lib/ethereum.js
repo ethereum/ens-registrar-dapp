d9cc0bc93e01b4d56edda79985a25bb705b744fc
Fernando Tiberti <fertiberti@gmail.com>
2017-01-03 17:46:24 -0300

    Fail early if ENS contract is not found
    Also, check that we're on the Ropsten network

--- app/imports/lib/ethereum.js
+++ app/imports/lib/ethereum.js
@@ -35,6 +35,35 @@ export default ethereum = (function() {
       check();
     });
   }
+  
+  function checkNetwork(web3) {
+    return new Promise((resolve, reject) => {
+      web3.eth.getBlock(0, function(e, res){
+        if (e) {
+          return reject(e)
+        }
+        let rejectMessage = 'Sorry, ENS is only available on the Ropsten testnet' +
+          ' network at the moment.'
+        switch(res.hash) {
+          case '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d':
+            network='ropsten';
+            resolve(web3);
+            break;
+          case '0x0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303':
+            network='morden';
+            reject(rejectMessage);
+            break;
+          case '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3':
+            network='main';
+            reject(rejectMessage);
+            break;
+          default:
+            network='private';
+            reject(rejectMessage);
+        }
+      });
+    })
+  }
 
   function initRegistrar(web3) {
     reportStatus('Initializing ENS registrar...');
@@ -43,6 +72,11 @@ export default ethereum = (function() {
         var ens = new ENS(web3, '0x112234455c3a32fd11230c42e7bccd4a84e02010');
         var registrar = new Registrar(web3);
         registrar.init();
+        let owner = registrar.ens.owner('eth')
+        if(owner !== '0xc68de5b43c3d980b0c110a77a5f78d3c4c4d63b4') {
+          throw 'Could not find ENS contract. Make sure your node' +
+            ' is synced to at least block 25409.';
+        }
         resolve({web3, ens, registrar});
       } catch(e) {
         reject('Error initialiting ENS registrar: ' + e);
@@ -76,6 +110,7 @@ export default ethereum = (function() {
       reportStatus('Connecting to Ethereum network...');
       return initWeb3()
         .then(checkConnection)
+        .then(checkNetwork)
         .then(initRegistrar)
         .then(setGlobals)
         .then(() => {
