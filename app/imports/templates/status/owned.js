d892358f10c56516d389ac699eda81a0dc6a007c
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2017-01-04 15:12:29 -0200

    refactored and simplified templates

--- app/imports/templates/status/owned.js
+++ app/imports/templates/status/owned.js
@@ -26,14 +26,18 @@ Template['status-owned'].helpers({
     var date = new Date(TemplateVar.get('entryData').registrationDate * 1000);
     return date.toLocaleString();
   },
+  deedValue() {
+    var val = TemplateVar.get('entryData').deed.balance;
+    return web3.fromWei(val.toFixed(), 'ether');
+  },
+  deedValueIsMin() {
+    var val = TemplateVar.get('entryData').deed.balance;
+    return val.toFixed() == 10000000000000000;
+  },
   renewalDate() {
     var years = 365 * 24 * 60 * 60 * 1000;
     var date = new Date(TemplateVar.get('entryData').registrationDate * 1000 + 2 * years);
     return date.toLocaleDateString();
-  },
-  deedValue() {
-    var val = TemplateVar.get('entryData').deed.balance;
-    return web3.fromWei(val.toFixed(), 'ether');
   },
   highestBid() {
     var val = TemplateVar.get('entryData').highestBid;
@@ -41,5 +45,13 @@ Template['status-owned'].helpers({
   },
   content() {
     return TemplateVar.get('content') == '0x' ? 'not set' : TemplateVar.get('content') ;
+  }
+})
+
+
+Template['aside-owned'].helpers({
+  deedValue() {
+    var val = Template.instance().data.entry.deed.balance;
+    return web3.fromWei(val.toFixed(), 'ether');
   }
 })
