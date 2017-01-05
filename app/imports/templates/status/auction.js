8b42bd0ccf7648eb12c574c7da5cd4b31b01b99e
Fernando Tiberti <fertiberti@gmail.com>
2017-01-05 12:58:54 -0300

    Can submit bids

--- app/imports/templates/status/auction.js
+++ app/imports/templates/status/auction.js
@@ -1,8 +1,31 @@
 import { registrar } from '/imports/lib/ethereum';
 
 Template['status-auction'].events({
-  'click .bid': function submitBid() {
+  'submit .new-bid'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const bidAmount = target.bidAmount.value;
+    const depositAmount = target.depositAmount.value;
+    const name = Session.get('searched');
+    let accounts = EthAccounts.find().fetch();
     
+    if (accounts.length == 0) {
+      alert('No accounts added to Dapp');
+    } else {
+      let bid = registrar.shaBid(name, accounts[0].address, bidAmount, 'asdf');
+      registrar.newBid(bid, {
+        value: depositAmount, 
+        from: accounts[0].address,
+        gas: 500000
+      }, (err, res) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(res)
+        }
+      });
+    }
   }
 })
 
