136d330f447a3aa2bbb1be9e2e35159853506075
Fernando Tiberti <fertiberti@gmail.com>
2017-01-06 06:56:03 -0300

    Bids have all the details

--- app/imports/templates/status/auction.js
+++ app/imports/templates/status/auction.js
@@ -3,26 +3,36 @@ import { registrar } from '/imports/lib/ethereum';
 Template['status-auction'].events({
   'submit .new-bid'(event) {
     event.preventDefault();
-
+    
     const target = event.target;
     const bidAmount = target.bidAmount.value;
     const depositAmount = target.depositAmount.value;
     const name = Session.get('searched');
+    const masterPassword = 'asdf';
     let accounts = EthAccounts.find().fetch();
     
     if (accounts.length == 0) {
       alert('No accounts added to Dapp');
     } else {
-      let bid = registrar.shaBid(name, accounts[0].address, bidAmount, 'asdf');
+      let bid = registrar.shaBid(name, accounts[0].address, bidAmount,
+        masterPassword);//todo: derive the salt using the password and the name
       registrar.newBid(bid, {
         value: depositAmount, 
         from: accounts[0].address,
         gas: 500000
       }, (err, res) => {
         if (err) {
-          console.log(err)
+          alert(err)
         } else {
-          MyBids.insert({txid:res});
+          MyBids.insert({
+            txid: res,
+            name,
+            fullName: name + '.eth',
+            bidAmount,
+            depositAmount,
+            date: Date.now(),
+            masterPassword
+          });
           console.log(res);
         }
       });
@@ -32,6 +42,7 @@ Template['status-auction'].events({
 
 Template['aside-auction'].helpers({
   bids() {
-    return MyBids.find();
+    const name = Session.get('searched');
+    return MyBids.find({name: name});
   }
 })
