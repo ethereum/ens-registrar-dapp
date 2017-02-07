ad78d3019532627bf7370ea0ccfcb66f98af191c
Fernando Tiberti <fertiberti@gmail.com>
2017-02-06 22:05:19 -0300

    Set title in head only

--- app/imports/templates/layout/body.js
+++ app/imports/templates/layout/body.js
@@ -1,36 +1,6 @@
 import ethereum, { errors } from '/imports/lib/ethereum';
 
-/**
-Template Controllers
-
-@module Templates
-*/
-
-/**
-The home template
-
-@class [template] layout_body
-@constructor
-*/
-
-Template['layout_body'].helpers({
-    /**
-    Get the name
-
-    @method (name)
-    */
-
-    'name': function() {
-        return this.name || TAPi18n.__('dapp.home.defaultName');
-    },
-    'network': function() {
-      return Session.get('network');
-    }
-});
-
-// When the template is created
 Template['layout_body'].onCreated(function(){
-  Meta.setSuffix(TAPi18n.__("dapp.home.title"));
   ethereum.onStatusChange(status => {
     TemplateVar.set(this, 'networkError', false)
     TemplateVar.set(this, 'isReady', status.isReady)
