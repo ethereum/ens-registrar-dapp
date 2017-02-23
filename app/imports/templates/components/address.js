d7dfacc5234544856fb2925c2bd97e70a32598b7
Fernando Tiberti <fertiberti@gmail.com>
2017-02-23 17:14:14 -0300

    Addresses are resolved with reverse resolution

--- app/imports/templates/components/address.js
+++ app/imports/templates/components/address.js
@@ -1,6 +1,23 @@
+import { ens } from '/imports/lib/ethereum';
+
+Template['components_address'].onCreated(function() {
+  this.name = new ReactiveVar();
+  if (!this.data.addr) {
+    return;
+  }
+  ens.reverse(this.data.addr, (err, resolver) =>{
+    if (!err && resolver.name) {
+      this.name.set(resolver.name());
+    }
+  })
+})
+
 
 Template['components_address'].helpers({
   mine() {
     return web3.eth.accounts.filter((acc) => acc === this.addr).length > 0;
+  },
+  name() {
+    return Template.instance().name.get();
   }
 })
