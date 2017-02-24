2d0f1f40625ecc6824f0204e2d03d848e31d9cb8
Fernando Tiberti <fertiberti@gmail.com>
2017-02-24 13:13:07 -0300

    Validate reverse resolution before displaying the name

--- app/imports/templates/components/address.js
+++ app/imports/templates/components/address.js
@@ -1,5 +1,19 @@
 import { ens } from '/imports/lib/ethereum';
 
+function validateName(claimedAddr, name, cb) {
+  ens.resolver(name, (err, res) => {
+    if (err) {
+      return cb(err);
+    }
+    res.addr((err, actualAddr) => {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, claimedAddr == actualAddr);
+    });
+  });
+}
+
 Template['components_address'].onCreated(function() {
   const template = Template.instance();
   template.autorun(() => {
@@ -11,9 +25,11 @@ Template['components_address'].onCreated(function() {
     ens.reverse(addr, (err, resolver) =>{
       if (!err && resolver.name) {
         resolver.name((err, name) => {
-          if (!err) {
-            TemplateVar.set(template, 'name', name);
-          }
+          validateName(addr, name, (err, isValid) => {
+            if (!err && isValid) {
+              TemplateVar.set(template, 'name', name);
+            }
+          });
         });
       }
     })
