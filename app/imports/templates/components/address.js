d5577ad281eff72933bcf0fe07603913a2e7383b
Fernando Tiberti <fertiberti@gmail.com>
2017-02-24 11:23:03 -0300

    Don't use autorun on components_address

--- app/imports/templates/components/address.js
+++ app/imports/templates/components/address.js
@@ -1,19 +1,16 @@
 import { ens } from '/imports/lib/ethereum';
 
 Template['components_address'].onCreated(function() {
-  this.autorun(() => {
-    const addr = Template.currentData().addr;
-    const template = Template.instance();
-    
-    if (!template.data.addr) {
-      return;
+  const template = Template.instance();
+  const addr = template.data.addr;
+  if (!template.data.addr) {
+    return;
+  }
+  ens.reverse(template.data.addr, (err, resolver) =>{
+    if (!err && resolver.name) {
+      TemplateVar.set(template, 'name', resolver.name());
     }
-    ens.reverse(template.data.addr, (err, resolver) =>{
-      if (!err && resolver.name) {
-        TemplateVar.set(template, 'name', resolver.name());
-      }
-    })
-  });
+  })
 })
 
 Template['components_address'].helpers({
