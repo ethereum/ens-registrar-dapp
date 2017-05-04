3c48d207505ce044850b4475a1322b2502411862
Alex Van de Sande <avsa@ethereum.org>
2017-05-04 11:47:10 -0300

    fix undefined mode on name and others (#79)

--- app/imports/lib/bids.js
+++ app/imports/lib/bids.js
@@ -6,6 +6,13 @@ export function updatePendingBids(name) {
   bids.forEach(bid => {
     registrar.contract.sealedBids.call(bid.owner, bid.shaBid, (err, result) => {
       if (!err && result !== '0x0000000000000000000000000000000000000000') {
+        // check for duplicates 
+        var dupBid = MyBids.find({secret:bid.secret}).fetch();
+        if (dupBid && dupBid.name == bid.name && dupBid.secret == bid.secret && dupBid.hash == bid.hash) {
+          console.log('Tried to insert duplicate bid!', dupBid);
+          return;
+        }
+
         //bid successfully submitted
         MyBids.insert(bid);
         PendingBids.remove({id: bid._id});
