8c5344808c7e374fdb02e6144ff952e9f4243d6c
Alex Van de Sande <avsa@ethereum.org>
2017-06-20 14:41:49 -0300

    Two column layout flexbox (#169)
    * flexbox
    
    * build

--- app/imports/templates/views/names.js
+++ app/imports/templates/views/names.js
@@ -11,8 +11,14 @@ Template['view_names'].helpers({
   active() {
     return Names.find({watched:true, mode: {$in: ['owned', 'auction', 'reveal']}},{sort: {registrationDate: 1}});
   },
-  inactive() {
-    return Names.find({watched:true, mode: {$in: ['not-yet-available', 'open']}},{sort: {availableDate: 1}});  },
+  notYetAvailable() {
+    return Names.find({watched:true, mode: {$in: ['not-yet-available']}},{sort: {availableDate: 1}});  
+  },
+  mode(mode) {
+    if (mode == 'owned') return Names.find({watched:true, mode: 'owned'},{sort: {name: 1}});  
+
+    return Names.find({watched:true, mode: mode},{sort: {availableDate: 1}});  
+  },
   watchesNames() {
     return Names.find({watched:true}).count() > 0;
   }, 
